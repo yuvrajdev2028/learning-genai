f7a1c1857ad7a1d3b563c0ffd4bbccb7db2e8181
yuvrajdev1920 <yuvraj.pratap1920@gmail.com>
2026-03-22 18:28:06 +0530

    basic error types in python'

---
+++ err_handling/01_err_basic.py
@@ -0,0 +1,41 @@
+# Index Error -> IndexError: list index out of range
+# games = ['cricket','football']
+
+# print(games[2])
+
+# ---------------------------------------------------------------------------------------
+
+# Key Error -> KeyError: 'England'
+# player_country_map = {
+#     "Australia":['Michael Clarke', 'Steve Smith', 'Brett Lee', 'Shane Warne', 'Shane Watson'],
+#     "India":['Sachin Tendulkar', 'Virat Kohli', 'MS Dhoni', 'Shikhar Dhawan', 'Rohit Sharma', 'Jasprit Bumrah', 'Kapil Dev']
+# }
+
+# print(player_country_map['India']) # Works Fine
+# print(player_country_map['England']) # Throws key error
+
+# ---------------------------------------------------------------------------------------
+
+# Zero Division Error -> ZeroDivisionError: division by zero
+# num1 = 10
+# num2 = 0
+# res = num1 / num2
+# print(res)
+
+# ---------------------------------------------------------------------------------------
+
+# Type Error
+
+# print('2' + 2) # TypeError: can only concatenate str (not "int") to str
+
+# a = 10
+# a() # TypeError: 'int' object is not callable
+
+# teams = ['Chennai', 'Mumbai', 'Kolkata']
+# print(teams['1']) # TypeError: list indices must be integers or slices, not str
+
+# ---------------------------------------------------------------------------------------
+
+# Name Error -> NameError: name 'non_existing_variable' is not defined
+
+# print(non_existing_variable)
